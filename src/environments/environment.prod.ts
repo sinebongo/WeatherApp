74bf1a642001a0c25fd6328ea1a0ac2b34098692
Sinebongo Setona <sinebongo.setona9@gmail.com>
2023-06-13 03:57:19 +0200

    dist folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=krugersdorp',
+  XRapidAPIKeyHeader: 'X-RapidAPI-Key',
+  XRapidAPIHostHeader: 'X-RapidAPI-Host',
+  XRapidAPIKey: 'a60ca7c267msh373091a6c48cd65p1671f0jsn6aed35526059',
+  XRapidAPIHost: 'weather-by-api-ninjas.p.rapidapi.com'
+
 };
